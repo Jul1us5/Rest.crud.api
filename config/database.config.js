56e3bed1784e7d8dfd38b98dc26f75e4704b11f1
Julius <julius.vlasovas@gmail.com>
2021-03-08 20:34:57 +0200

    first commit

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    url: 'https://jul1us5.herokuapp.com/'
-    // url: 'mongodb+srv://new_user1:2NmfEKpVsjZsXZ3@cluster0.q1dzw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    url: 'mongodb+srv://new1:new1@cluster0.emzrs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
