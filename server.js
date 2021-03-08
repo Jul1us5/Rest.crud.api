0420386a1f709f7039350f3f0f88e8b70fb88985
Julius <julius.vlasovas@gmail.com>
2021-03-08 15:38:12 +0200

    step 2?

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const importData = require('./data.json');
 
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello!!!")
+    res.send("Hello!!!");
+})
+
+app.get("/easynotes", (req, res) => {
+    res.send(importData);
 })
 
 app.listen(port, () => {
